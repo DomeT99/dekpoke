98a88e44b270a4a9528723929ee603e5bb1f0aff
Domenico Tenace <domenicotenace99@gmail.com>
2023-02-21 15:33:08 +0100

    feat: style partial navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,24 +1,95 @@
 <script setup lang="ts">
-	const menu = [
-		{ title: "Pokemon", path: "/" },
-		{ title: "Moves", path: "/moves" },
-		{ title: "Items", path: "/items" },
-		{ title: "Machines", path: "/machines" },
-	];
+const menu = [
+  { title: "Pokemon", path: "/" },
+  { title: "Moves", path: "/moves" },
+  { title: "Items", path: "/items" },
+  { title: "Machines", path: "/machines" },
+];
 </script>
 
 <template>
-	<nav
-		class="p-4 bg-[var(--primary-color)] text-[var(--quaternary-color)] border-b-2 border-b-[var(--secondary-color)]"
-	>
-		<ul
-			class="flex md:space-x-8 flex-col space-y-4 md:flex-row md:space-y-0 md:text-lg text-sm"
-		>
-			<span
-				v-for="voice in menu"
-				class="border-2 p-1 border-[var(--secondary-color)] rounded-lg bg-[var(--quaternary-color)] text-[var(--secondary-color)] w-24 text-center border-b-[5px]"
-				><NuxtLink :to="voice.path">{{ voice.title }}</NuxtLink></span
-			>
-		</ul>
-	</nav>
+  <nav
+    class="bg-[var(--primary-color)] text-[var(--quaternary-color)] border-b-2 border-b-[var(--secondary-color)]"
+  >
+    <div class="mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <button
+            type="button"
+            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-[var(--tertiary-color)] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[var(--secondary-color)]"
+            aria-controls="mobile-menu"
+            aria-expanded="false"
+          >
+            <span class="sr-only"></span>
+
+            <svg
+              class="block h-6 w-6 text-[var(--secondary-color)]"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+
+            <svg
+              class="hidden h-6 w-6 text-[var(--secondary-color)]"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+        >
+          <div class="flex flex-shrink-0 items-center">
+            <img
+              class="block h-8 w-auto lg:hidden"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Dekpoke Logo"
+            />
+            <img
+              class="hidden h-8 w-auto lg:block"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Dekpoke Logo"
+            />
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <span
+                v-for="voice in menu"
+                class="cursor-pointer font-semibold border-2 p-1 border-[var(--secondary-color)] rounded-lg bg-[var(--quaternary-color)] text-[var(--secondary-color)] w-24 text-center border-b-[5px]"
+                ><NuxtLink :to="voice.path">{{ voice.title }}</NuxtLink></span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="sm:hidden" id="mobile-menu">
+      <div class="space-y-1 px-2 pt-2 pb-3 mb-4">
+        <span
+          v-for="voice in menu"
+          class="cursor-pointer w-full mt-4 block font-semibold border-2 p-1 border-[var(--secondary-color)] rounded-lg bg-[var(--quaternary-color)] text-[var(--secondary-color)] w-24 text-center border-b-[5px]"
+          ><NuxtLink :to="voice.path">{{ voice.title }}</NuxtLink></span
+        >
+      </div>
+    </div>
+  </nav>
 </template>
