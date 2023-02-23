b6de3dd15cc66b7ba78ccc5be44ddd050077036a
Domenico Tenace <domenicotenace99@gmail.com>
2023-02-23 14:19:50 +0100

    feat: grid in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <section class="container mx-auto">
+    <div class="text-center mb-[3rem] mt-[3rem]">
+      <h1>List of Pokemon</h1>
+    </div>
+    <div class="grid lg:grid-cols-6 md:grid-cols-3 gap-4">
+      <div>01</div>
+      <div>01</div>
+      <div>01</div>
+      <div>01</div>
+      <div>01</div>
+      <div>01</div>
+    </div>
+  </section>
 </template>
