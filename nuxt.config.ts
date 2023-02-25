94d5dd3c9145c154b42429685efb59c755de563b
Domenico <domenicotenace99@gmail.com>
2023-02-25 19:20:24 +0100

    feat: add favicon and pwa settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,31 @@
 // @ts-nocheck
 export default defineNuxtConfig({
 	modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
-	pwa: {},
+	pwa: {
+		registerType: "autoUpdate",
+		manifest: {
+			name: "Nuxt Vite PWA",
+			short_name: "NuxtVitePWA",
+			theme_color: "#ffffff",
+			icons: [
+				{
+					src: "pwa-192x192.png",
+					sizes: "192x192",
+					type: "image/png",
+				},
+				{
+					src: "pwa-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+				},
+				{
+					src: "pwa-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+					purpose: "any maskable",
+				},
+			],
+		}
+	},
 	css: ["assets/scss/global.scss"],
 });
