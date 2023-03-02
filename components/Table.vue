caab3808943738ff662da827d9b6bd247b39b377
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-02 10:54:59 +0100

    feat: define generic table component

--- components/Table.vue
+++ components/Table.vue
@@ -1,48 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Table {
+  tableHeader: string[];
+  data: object[];
+}
+const props = defineProps<Table>();
+
+</script>
 
 <template>
-	<div class="flex justify-center mx-auto">
-		<div class="flex flex-col">
-			<table
-				class="divide-y divide-y-2 divide-[var(--secondary-color)] border-2 rounded-lg border-[var(--secondary-color)]"
-			>
-				<thead class="bg-gray-50">
-					<tr class="bg-[var(--primary-color)]">
-						<th class="px-6 py-2 sm:text-lg text-xs text-[var()]">Moves</th>
-						<th
-							class="px-6 py-2 sm:text-lg text-xs text-[var(--secondary-color)]"
-						>
-							Power
-						</th>
-						<th
-							class="px-6 py-2 sm:text-lg text-xs text-[var(--secondary-color)]"
-						>
-							Accuracy
-						</th>
-						<th
-							class="px-6 py-2 sm:text-lg text-xs text-[var(--secondary-color)]"
-						>
-							Type
-						</th>
-					</tr>
-				</thead>
-				<tbody class="bg-white divide-y divide-[var(--tertiary-color)]">
-					<tr class="text-center">
-						<td class="px-6 py-4 text-sm text-[var(--secondary-color)]">
-							Heartquake
-						</td>
-						<td class="px-6 py-4">
-							<div class="text-sm text-[var(--secondary-color)]">90</div>
-						</td>
-						<td class="px-6 py-4">
-							<div class="text-sm text-[var(--secondary-color)]">100</div>
-						</td>
-                        <td class="px-6 py-4">
-							<div class="text-sm text-[var(--secondary-color)]">100</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
+  <div class="flex justify-center mx-auto">
+    <div class="flex flex-col">
+      <table
+        class="divide-y divide-y-2 divide-[var(--secondary-color)] border-4 rounded-3xl border-[var(--secondary-color)]"
+      >
+        <thead class="bg-gray-50">
+          <tr class="bg-[var(--primary-color)]">
+            <th
+              v-for="head in props.tableHeader"
+              class="px-6 py-2 sm:text-lg text-xs text-[var()]"
+            >
+              {{ head }}
+            </th>
+          </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-[var(--tertiary-color)]">
+          <tr class="text-center" v-for="obj in props.data">
+            <td class="px-6 py-4 text-sm text-[var(--secondary-color)]" v-for="data in obj">
+              {{ data }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
--- pages/moves.vue
+++ pages/moves.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+let daa = [
+  {
+    h: "hh",
+    a: "dsa",
+    s: "aa",
+    d: "a",
+  },
+  {
+    h: "hh",
+    a: "dsa",
+    s: "aa",
+    d: "a",
+  },
+];
+</script>
 <template>
-	<section class="container mx-auto">
-		<header class="text-center mb-[3rem] mt-[3rem]">
-			<h1>Moves</h1>
-		</header>
-		<div>
-			<Table />
-		</div>
-	</section>
+  <section class="container mx-auto">
+    <header class="text-center mb-[3rem] mt-[3rem]">
+      <h1>Moves</h1>
+    </header>
+    <div>
+      <Table
+        :data="daa"
+        :table-header="['Moves', 'Power', 'Accuracy', 'Type']"
+      />
+    </div>
+  </section>
 </template>
