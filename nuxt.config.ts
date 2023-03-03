a0a20f2a7c4c9e838dc5e52f4d8748d99f955cd8
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-03 17:48:52 +0100

    feat: uninstall @vite-pwa/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,32 +6,6 @@ export default defineNuxtConfig({
     "@vite-pwa/nuxt",
     "nuxt-lodash",
   ],
-  pwa: {
-    registerType: "autoUpdate",
-    manifest: {
-      name: "Nuxt Vite PWA",
-      short_name: "NuxtVitePWA",
-      theme_color: "#ffffff",
-      icons: [
-        {
-          src: "pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-        {
-          src: "pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-      ],
-    },
-  },
   lodash: {
     prefix: "_",
     prefixSkip: ["string"],
