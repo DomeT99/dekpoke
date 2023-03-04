397dd031afd4ee12ec5f9d29f95afc3b56a7f75e
Domenico <domenicotenace99@gmail.com>
2023-03-04 16:53:53 +0100

    feat: add table in items page

--- pages/items.vue
+++ pages/items.vue
@@ -1,4 +1,87 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	let daa = [
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+		{
+			h: "hh",
+			a: "dsa",
+			s: "aa",
+			d: "a",
+		},
+	];
+</script>
 <template>
-  <div></div>
+	<section class="container mx-auto">
+		<header class="text-center mb-[3rem] mt-[3rem]">
+			<h1>Items</h1>
+		</header>
+
+		<div>
+			<Table :data="daa" :table-header="['Name', 'Cost', 'Effect', 'Sprite']" />
+		</div>
+	</section>
 </template>
