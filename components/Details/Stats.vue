c44db5b70957d4cc166dfb72a81276c9aad66a86
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-07 14:21:20 +0100

    feat: stats component partial styling

--- components/Details/Stats.vue
+++ components/Details/Stats.vue
@@ -1,6 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+let stats = [
+  { name: "HP", stat: "168" },
+  { name: "Attack", stat: "168" },
+  { name: "Defense", stat: "168" },
+  { name: "Special Atk.", stat: "168" },
+  { name: "Special Def.", stat: "168" },
+  { name: "Speed.", stat: "168" },
+];
+</script>
 <template>
   <Box>
-    <h1>C</h1>
+    <div class="grid grid-cols-2" v-for="i in stats">
+      <div>
+        <h2>{{ i.name }}</h2>
+      </div>
+      <div>
+        <h2>{{ i.stat }}</h2>
+      </div>
+    </div>
   </Box>
 </template>
